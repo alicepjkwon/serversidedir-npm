5d70f90184bb15179f7da45284585e05c5327e26
alicepjkwon <38158654+alicepjkwon@users.noreply.github.com>
2018-04-28 22:04:36 +0900

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,3 @@
+console.log('hello world');
+console.log('hello hello world');
+console.log(1+1);
---
+++ module.js
@@ -0,0 +1,6 @@
+const os = require('os');
+console.log('os');
+console.log(os.platform());
+console.log(os.loadavg());
+console.log(os.arch());
+console.log(os.hostname());
